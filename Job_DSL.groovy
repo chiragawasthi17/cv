e81c1961ddaa6499c0d0c7b78e49f15a6b018537
Chirag Awasthi <96935674+chiragawasthi17@users.noreply.github.com>
2023-04-12 22:03:20 +0530

    Update Job_DSL.groovy

--- Job_DSL.groovy
+++ Job_DSL.groovy
@@ -1,4 +1,4 @@
-job('First-CV-pipeline'){
+job('First-seed-CV-pipeline'){
   description("First job dsl job created via groovy syntax on $(new Date()),this project is job dsl")
   scm{git("https://github.com/chiragawasthi17/cv.git",master)}
   triggers{scm('* * * * *')}
