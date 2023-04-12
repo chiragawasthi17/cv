c65e3459d7c0f39a4432ee313fff06a65b630535
Chirag Awasthi <96935674+chiragawasthi17@users.noreply.github.com>
2023-04-12 22:01:07 +0530

    Create Job_DSL.groovy

---
+++ Job_DSL.groovy
@@ -0,0 +1,6 @@
+job('First-CV-pipeline'){
+  description("First job dsl job created via groovy syntax on $(new Date()),this project is job dsl")
+  scm{git("https://github.com/chiragawasthi17/cv.git",master)}
+  triggers{scm('* * * * *')}
+  steps{shell('docker build -t cv-app .', 'docker run -itd --name cv-app1 -p 8001:80 cv-app')}
+   
