d811c0b1af9c4dcb560c4cc43d8e81a48395d995
Chirag Awasthi <96935674+chiragawasthi17@users.noreply.github.com>
2023-04-12 22:23:12 +0530

    Update Job_DSL.groovy

--- Job_DSL.groovy
+++ Job_DSL.groovy
@@ -1,6 +1,16 @@
-job('First-seed-CV-pipeline'){
-  description("First job dsl job created via groovy syntax on $(new Date()),this project is job dsl")
-  scm{git("https://github.com/chiragawasthi17/cv.git",master)}
-  triggers{scm('* * * * *')}
-  steps{shell('docker build -t cv-app .', 'docker run -itd --name cv-app1 -p 8001:80 cv-app')}
-   
+job('job dsl')
+{
+    description('this is our first job dsl using apache groovy')
+    scm
+    {
+        git("https://github.com/chiragawasthi17/cv.git",master)
+    }
+    trigger
+    {
+        scm('* * * * *')
+    }
+    steps
+    {
+        shell('docker build -t cv-app1 .', 'docker run -itd --name cv-app2 -p 8001:80 cv-app1')
+    }
+}
